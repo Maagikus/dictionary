d2a50db102971e2691b46a7530d051998a8c2b83
Yan <maagikus@gmail.com>
2022-12-01 20:55:15 +0200

    added validation for AddForm component

--- src/components/AddForm/AddForm.js
+++ src/components/AddForm/AddForm.js
@@ -5,35 +5,42 @@ function AddForm({ setActive }) {
 	const dispatch = useDispatch()
 	const [word, setWord] = useState('')
 	const [translation, setTranslation] = useState('')
-
+	const [formErrorMassage, setFormErrorMassage] = useState('')
+	const [formError, setFormError] = useState(false)
 	const onSubmit = async (e) => {
 		e.preventDefault()
-
-		const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-		const data = await response.json()
-		const { text, audio } = data[0].phonetics[1]
-		const dictionaryData = {
-			word: word,
-			transcription: text ? text : '',
-			translation: translation,
-			audio: audio
+		if (word.length > 1 && translation.length > 1) {
+			const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+			if (response.status === 200) {
+				const data = await response.json()
+				const { text, audio } = data[0].phonetics[1]
+				const dictionaryData = {
+					word: word,
+					transcription: text ? text : '',
+					translation: translation,
+					audio: audio
+				}
+				dispatch(addUserDictionary(dictionaryData))
+				setWord('')
+				setTranslation('')
+				setActive(false)
+				setFormError(false)
+			}
+			if (response.status === 404) {
+				setFormError(true)
+				setFormErrorMassage('something was wrong, pleace check your word')
+			}
 		}
-		dispatch(addUserDictionary(dictionaryData))
-		setWord('')
-		setTranslation('')
-		setActive(false)
-
 	}
-
 	return (
-		<form onSubmit={e => {
-			onSubmit(e)
-
-		}} action="#" className="dictionary__form form-dictionary">
+		<form onSubmit={e => onSubmit(e)} action="#" className="dictionary__form form-dictionary">
 			<div className="form-dictionary__wrapper">
 				<div className="form-dictionary__item">
 					<label className="form-dictionary__lable">Word</label>
-					<input value={word} onChange={e => setWord(e.target.value)} autoComplete="off" type="text" name="form[]" data-error="Ошибка" placeholder="" className="form-dictionary__input input" />
+					<input value={word} onChange={e => {
+						setWord(e.target.value)
+						setFormError(false)
+					}} autoComplete="off" type="text" name="form[]" data-error="Ошибка" placeholder="" className="form-dictionary__input input" />
 				</div>
 				<div className="form-dictionary__item">
 					<label className="form-dictionary__lable">Translation</label>
@@ -41,6 +48,7 @@ function AddForm({ setActive }) {
 				</div>
 			</div>
 			<button className="form-dictionary__button button">Add</button>
+			{formError ? <div>{formErrorMassage}</div> : null}
 		</form>
 	)
 }
